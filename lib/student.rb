ff8dd1601a658e9106daedac119262d58d337fab
Issa Nyale <me@email.com>
2023-02-15 11:05:14 +0300

    Completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+
+    def first_name
+        first_name = "Steve"
+
+    end
+
+    def last_name
+        last_name = "Jobs"
+    end
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge_string)
+        @knowledge << knowledge_string
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+    def first_name=(name)
+        @first_name = name
+    end
+
+    def last_name=(name)
+        @last_name = name
+    end
+
+    def first_name
+        "Tracy"
+    end
+
+    def last_name
+        "Morgan"
+    end
+end
